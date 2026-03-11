40902c5836058df736fa3673f7d6d26cfb019448
ruben-furlan <42449800+ruben-furlan@users.noreply.github.com>
2026-03-11 18:05:05 +0100

    feat: add FAQ section with accordion in landing page

--- src/app/features/home/home-page/home-page.ts
+++ src/app/features/home/home-page/home-page.ts
@@ -4,11 +4,12 @@ import { LocationComponent } from '../../location/location';
 import { BookingFormComponent } from '../../booking-form/booking-form';
 import {NgOptimizedImage} from '@angular/common';
 import { RevealOnScrollDirective } from '../../../shared/directives/reveal-on-scroll.directive';
+import { FaqComponent } from '../../faq/faq';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [Testimonials, LocationComponent, BookingFormComponent, NgOptimizedImage, RevealOnScrollDirective ],
+  imports: [Testimonials, LocationComponent, BookingFormComponent, NgOptimizedImage, RevealOnScrollDirective, FaqComponent],
   templateUrl: './home-page.html',
   styleUrls: ['./home-page.css']
 })
