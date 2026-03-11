30ae4f1119b46f51826b296eb74b079510efd801
ruben-furlan <42449800+ruben-furlan@users.noreply.github.com>
2026-03-11 16:58:22 +0100

    Mueve política de cookies a página accesible desde footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './core/header/header';
 import { FooterComponent } from './core/footer/footer';
 import { FloatingWhatsappButtonComponent } from './core/floating-whatsapp-button/floating-whatsapp-button';
-import { CookieConsentComponent } from './core/cookie-consent/cookie-consent';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, FooterComponent, FloatingWhatsappButtonComponent, CookieConsentComponent],
+  imports: [RouterOutlet, Header, FooterComponent, FloatingWhatsappButtonComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
