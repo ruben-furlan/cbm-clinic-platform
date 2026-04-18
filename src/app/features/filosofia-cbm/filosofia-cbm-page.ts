150badca839b1849f1a7a11c82c6a921873e4ee4
ruben-furlan <42449800+ruben-furlan@users.noreply.github.com>
2026-04-18 07:52:43 +0200

    feat: add Filosofía CBM editorial page

---
+++ src/app/features/filosofia-cbm/filosofia-cbm-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-filosofia-cbm-page',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './filosofia-cbm-page.html',
+  styleUrl: './filosofia-cbm-page.css'
+})
+export class FilosofiaCbmPage {}
